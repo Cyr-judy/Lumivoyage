a17a89fc9aee2ee30a0a9d4a3a9c22c29d537fb8
Judy <Chenyunru1220@outlook.com>
2025-05-15 01:19:30 +0800

    feat：增加游戏开始菜单

---
+++ Assets/Scripts/MainMenu/DialogController.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+
+public class DialogController : MonoBehaviour
+{
+    public GameObject Team;
+    public GameObject Credits;
+    public GameObject About;
+
+    // 打开主弹窗
+    public void ShowTeam()
+    {
+        Team.SetActive(true);
+    }
+
+    // 打开子弹窗
+    public void ShowCredits()
+    {
+        Credits.SetActive(true);
+        Team.SetActive(false);
+    }
+
+    // 关闭子弹窗
+    public void CloseCredits()
+    {
+        Credits.SetActive(false);
+        Team.SetActive(true);
+    }
+
+    // 关闭主弹窗（如果需要）
+    public void CloseTeam()
+    {
+        Team.SetActive(false);
+    }
+
+    public void ShowAbout()
+    {
+        About.SetActive(true);
+    }
+
+    public void CloseAbout()
+    {
+        About.SetActive(false);
+    }
+}
+
