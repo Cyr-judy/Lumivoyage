623937e8acf848a8986bbd876756dfdc5f003602
Judy <Chenyunru1220@outlook.com>
2025-05-21 00:33:01 +0800

    feat：增加leadin，增加leadin到homeinside的场景切换

---
+++ Assets/Scripts/LeadIn/LeadInPlayer.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.Video;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LeadInPlayer : MonoBehaviour
+{
+    public GameObject continueButton;  // 在 Inspector 中拖入按钮对象
+    public string nextSceneName;
+
+    private VideoPlayer vp;
+
+    void Start()
+    {
+        vp = GetComponent<VideoPlayer>();
+        vp.loopPointReached += OnVideoEnd;
+        continueButton.SetActive(false); // 初始隐藏按钮
+    }
+
+    void OnVideoEnd(VideoPlayer vp)
+    {
+        vp.Pause(); // 停在最后一帧
+        continueButton.SetActive(true); // 显示按钮
+    }
+
+    // 可挂在按钮 OnClick 事件上
+    public void OnContinueButtonClicked()
+    {
+        SceneManager.LoadScene(nextSceneName);
+    }
+}
+
